933e42fe31a31e0e14b78d5fe5236af5ff9e6fd0
Vladislav-ctrl <vlad.pashkevich.02@mail.ru>
2021-12-08 18:57:13 +0300

    spp_lab4

---
+++ spp_lab4/OSHandle.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace spp_lab4
+{
+    class OSHandle:IDisposable
+    {
+        [DllImport("kernel32.dll")] private static extern bool CloseHandle(IntPtr handle);
+        public IntPtr Handle { get; set; }
+
+        private bool disposed = false;
+
+        public OSHandle(IntPtr handle)
+        {
+            this.Handle = handle;
+        }
+
+        ~OSHandle(){
+            Dispose(false);
+        }
+
+        /*
+        protected override void Finalize()
+        {
+
+        }
+        */
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+
+                }
+                if (CloseHandle(Handle))
+                {
+                    Console.WriteLine("Handle " + Handle + " was disposed");
+                    Handle=IntPtr.Zero;
+                }
+                disposed=true;       
+            }
+        }
+
+    }
+}
---
+++ spp_lab4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using Microsoft.Win32.SafeHandles;
+
+namespace spp_lab4
+{
+    class Program
+    {
+        public const uint GENERIC_READ = 0x80000000;
+        public const uint OPEN_EXISTING = 3;
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        static extern SafeFileHandle CreateFile(string lpFileName, uint dwDesiredAccess,
+        uint dwShareMode, IntPtr lpSecurityAttributes, uint dwCreationDisposition,
+        uint dwFlagsAndAttributes, IntPtr hTemplateFile);
+
+        static void Main(string[] args)
+        {
+
+            SafeFileHandle file = CreateFile("D:/1/11.txt", GENERIC_READ, 0, IntPtr.Zero, OPEN_EXISTING, 0, IntPtr.Zero);
+
+            using(OSHandle oshandle = new OSHandle(file.DangerousGetHandle())) { 
+                Console.WriteLine(oshandle.Handle);
+            }
+
+            GC.Collect();
+
+            Console.ReadKey();
+        }
+
+        
+    }
+}
