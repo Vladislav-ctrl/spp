51597d5ca23ed7dd39ff34ae8b2ada80a1d386fc
Vladislav-ctrl <vlad.pashkevich.02@mail.ru>
2021-12-08 18:44:35 +0300

    spp_lab1

---
+++ spp_lab1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace spp_lab1
+{
+    public class Program
+    {
+        static object locker = new object();
+        static void method1()
+        {
+            Console.WriteLine("Задача 1");
+            Thread.Sleep(200);
+        }
+
+        static void method2()
+        {
+            Console.WriteLine("Задача 2");
+            Thread.Sleep(200);
+        }
+
+        static void method3()
+        {
+            Console.WriteLine("Задача 3");
+            Thread.Sleep(200);
+        }
+
+        static void Main(string[] args)
+        {
+            TaskQueue taskQueue = new TaskQueue(3);
+            taskQueue.EnqueueTask(method2);
+            taskQueue.EnqueueTask(method3);
+            taskQueue.EnqueueTask(method1);
+            taskQueue.EnqueueTask(method2);
+        }
+    }
+}
---
+++ spp_lab1/TaskQueue.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace spp_lab1
+{
+    public class TaskQueue
+    {
+        public delegate void TaskDelegate();
+        Queue<TaskDelegate> queue= new Queue<TaskDelegate>();
+        static object locker = new object();
+        public TaskQueue(int count)
+        {
+            for(int i=0; i < count; i++)
+            {
+                Thread thread = new Thread(task);
+                thread.Start();
+            }
+        }
+
+        public void task()
+        {
+            while (true)
+            {
+                TaskDelegate newTask=null;
+                lock (locker)
+                {
+                    if (queue.Count != 0)
+                    {
+                        newTask = queue.Dequeue();
+                    }
+                }
+                if(newTask!=null)newTask();
+            }
+        }
+
+        public void EnqueueTask(TaskDelegate task)
+        {
+            queue.Enqueue(task);
+        }
+    }
+}
