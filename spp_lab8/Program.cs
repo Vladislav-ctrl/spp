e30db99306fa3b18b93a7be8ed47b37f21bcebf4
Vladislav-ctrl <vlad.pashkevich.02@mail.ru>
2021-12-08 18:59:42 +0300

    spp_lab8

---
+++ spp_lab8/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace spp_lab8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = null;
+            bool check = true;
+            while (check)
+            {
+                Console.WriteLine("введите путь к сборке");
+                path = Console.ReadLine();
+                if (File.Exists(path))
+                {
+                    check = false;
+                }
+                else
+                {
+                    Console.WriteLine("Некорректный ввод");
+                }
+            }
+            Assembly asm = Assembly.LoadFrom(path);
+            Type[] types = asm.GetTypes();
+            for (int i = 0; i < types.Length; i++)
+            {
+                if (types[i].IsPublic)
+                {
+                    object[] attributes = types[i].GetCustomAttributes(false);
+                    for (int j = 0; j < attributes.Length; j++)
+                    {
+                        if (attributes[j].GetType().Name == "ExportClass")
+                        {
+                            Console.WriteLine(types[i].Name);
+                            break;
+                        }
+                    }
+                    //Console.WriteLine(types[i].FullName);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
