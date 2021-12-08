73592618c944915c88cc5bf65f1e49f1c85487ba
Vladislav-ctrl <vlad.pashkevich.02@mail.ru>
2021-12-08 18:49:14 +0300

    spp_lab2

---
+++ spp_lab2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Threading;
+using System.IO;
+
+
+namespace spp_lab2
+{
+    class Program
+    {
+        static void copy(string file_path,string path_copy_to)
+        {
+            FileInfo fileInfo = new FileInfo(file_path);
+            fileInfo.CopyTo(path_copy_to);
+            Thread.Sleep(200);
+        }
+
+        static void Main(string[] args)
+        {
+            string path_from=null;
+            bool check = true;
+            while (check) { 
+                Console.WriteLine("введите путь к исходному каталогу");
+                path_from = Console.ReadLine();
+                if (Directory.Exists(path_from))
+                {
+                    check = false;
+                }
+                else
+                {
+                    Console.WriteLine("Некорректный ввод");
+                }
+            }
+            check = true;
+            string path_to = null;
+            while (check)
+            {
+                Console.WriteLine("введите путь к целевому каталогу");
+                path_to = Console.ReadLine();
+                if (Directory.Exists(path_to))
+                {
+                    check = false;
+                }
+                else {
+                    Console.WriteLine("Некорректный ввод");
+                }
+            }
+            string[] files = Directory.GetFiles(path_from);
+            for (int i = 0; i < files.Length; i++)
+            {
+                Console.WriteLine(files[i]);
+            }
+            Console.WriteLine("from:" + path_from + "  to:" + path_to);
+            //Console.WriteLine(Directory.Exists(path_from));
+            //Console.ReadKey();
+            
+            
+            TaskQueue taskQueue = new TaskQueue(3,path_to);
+            for (int i = 0; i < files.Length; i++)
+            {
+                taskQueue.EnqueueFile(files[i]);
+            }            
+        }
+    }
+}
+
---
+++ spp_lab2/TaskQueue.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.IO;
+
+namespace spp_lab2
+{
+    class TaskQueue
+    {
+        public delegate void TaskDelegate(string file_path, string path_copy_to);
+        Queue<string> file_queue = new Queue<string>();
+        string path_copy_to;
+        int count_of_files = 0;
+        static object locker = new object();
+        public TaskQueue(int count,string path_copy_to)
+        {
+            this.path_copy_to = path_copy_to;
+            for (int i = 0; i < count; i++)
+            {
+                Thread thread = new Thread(task);
+                thread.Start();
+            }
+        }
+
+        public void task()
+        {
+            bool last = false;
+            while (true)
+            {
+                string file = null;
+                lock (locker)
+                {
+                    if (file_queue.Count != 0)
+                    {
+                        file = file_queue.Dequeue();
+                        if (file_queue.Count == 0) last = true;
+                    }
+                }
+                if (file != null)
+                {
+                    Console.WriteLine(Thread.CurrentThread.GetHashCode());
+                    FileInfo fileInfo = new FileInfo(file);
+                    fileInfo.CopyTo(path_copy_to+"//"+fileInfo.Name);
+                    count_of_files++;
+                    if (last) Console.WriteLine("Количесвто скопированных файлов:"+count_of_files);
+                    Thread.Sleep(100);
+                };
+            }
+        }
+
+        public void EnqueueFile(string file)
+        {
+            file_queue.Enqueue(file);
+        }
+    }
+}
