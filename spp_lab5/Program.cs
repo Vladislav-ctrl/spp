29ffac56a5dcec056ebb366455a75c32bdfe97c3
Vladislav-ctrl <vlad.pashkevich.02@mail.ru>
2021-12-08 18:58:22 +0300

    spp_lab5

---
+++ spp_lab5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace spp_lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = null;
+            bool check = true;
+            while (check)
+            {
+                Console.WriteLine("введите путь к сборке");
+                path = Console.ReadLine();
+                if (File.Exists(path))
+                {
+                    check = false;
+                }
+                else
+                {
+                    Console.WriteLine("Некорректный ввод");
+                }
+            }
+            Assembly asm = Assembly.LoadFrom(path);
+            //Console.WriteLine(asm.FullName);
+            Type[] types = asm.GetTypes();
+            for (int i = 0; i < types.Length; i++)
+            {
+                if (types[i].IsPublic)
+                {
+                    Console.WriteLine(types[i].FullName);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
